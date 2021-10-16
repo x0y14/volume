b049c7f5a3463fc05665a6b524102ed51987bd43
x0y14 <ya01ma08@gmail.com>
2021-10-17 01:30:17 +0900

    シンプルな構文テストを追加

--- src/volc/vbin_gen/tokenizer_test.go
+++ src/volc/vbin_gen/tokenizer_test.go
@@ -1,1 +1,18 @@
 package vbin_gen
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewTokenizer(t *testing.T) {
+	tokenizer := NewTokenizer("main:\n \tcp \"hello\", reg_a\n\tadd 1, reg_b ; src dst\n\tsub 1.3 reg_c\n\texit")
+	tokens, err := tokenizer.Tokenize()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, token := range *tokens {
+		fmt.Printf("%v\n", token.String())
+	}
+}
