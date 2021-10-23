f8e708373ec6bd1c0fcca2968b02d8d66a66ca7d
x0y14 <ya01ma08@gmail.com>
2021-10-24 01:20:05 +0900

    未実装エラーを追加

--- src/vcc/compiler/parser/error.go
+++ src/vcc/compiler/parser/error.go
@@ -5,3 +5,7 @@ import "fmt"
 func SyntaxErr(section string, expected string, actual string) error {
 	return fmt.Errorf("syntax error(%v): expected %v, but got %v", section, expected, actual)
 }
+
+func NotYetImplErr(section string, text string) error {
+	return fmt.Errorf("not yet implemented(%v): %v", section, text)
+}
